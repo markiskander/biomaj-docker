5cfdac233f7401ead83876d20347911b135efe35
Mark Iskander <motiontracker@gmail.com>
2015-09-24 14:16:18 -0500

    modified startup to only wait for as long as necessary for the database service to start

--- startup.sh
+++ startup.sh
@@ -6,7 +6,13 @@ sed -i -e 's@mongodb://localhost:27017@mongodb://'$MONGO_PORT_27017_TCP_ADDR':'$
 cd /home/bio/biomaj/biomaj/bin
 python biomaj-cli.py --config /home/bio/biomaj/global.properties &
 cd /home/bio/biomaj/biomaj-watcher/db
-sleep 20
+
+#wait until mongo service is ready to go, then proceed
+while [ `nc -vz $MONGO_PORT_27017_TCP_ADDR $MONGO_PORT_27017_TCP_PORT 2>&1 | grep 'succeeded!' | wc -l` -eq '0' ]
+do
+	sleep 1
+done
+
 python seed.py --config /home/bio/biomaj/global.properties --user bio --pwd maj &
 cd /home/bio/biomaj/biomaj-watcher
 su -c  'pserve development.ini' -s /bin/bash bio &
